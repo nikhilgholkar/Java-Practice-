e093360ff43ba01146b2371e342aea7555462134
Nikhil Gholkar <nikhilgholkar74@gmail.com>
2024-07-02 12:42:35 +0530

    add increament decreament operator code

---
+++ incDec/incDec.java
@@ -0,0 +1,14 @@
+public class incDec {
+
+public static void main(String args[])
+{
+    int x=6;
+    int y=x++;
+    System.out.println("x= "+x+" y= "+y);
+    char i='A';
+    char b=++i;
+    System.out.println(b);
+}
+
+
+}
